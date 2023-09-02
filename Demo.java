b524588d76178f42554c1d2f9302efecfcae0b99
Akash <akash300383@gmail.com>
2023-09-02 11:40:46 +0530

    done addition of two number

--- Demo.java
+++ Demo.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class Demo {
 	public static void main(String args[]) {
-		System.out.println("Welcome to bootcamp java batch");
-		System.out.println("Welcome once again we are learning CI and CD tool");
+		int a=10;
+		int b=20;
+		int sum = a+b;
+		System.out.println("Sum of two number is "+sum);
 	}
 }
