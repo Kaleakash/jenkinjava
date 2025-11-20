665eea4d4dbe36ebcaf2b80f0037d136104bc30f
Akash <akash300383@gmail.com>
2025-11-20 15:17:58 +0530

    error resolved

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-    System.out.println("This is Simple Java Project);
+    System.out.println("This is Simple Java Project");
     }
 }
